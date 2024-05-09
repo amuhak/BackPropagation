7528ff7f9f827865bb30916c1eada42b99055439
Amulya Jain <amuly.jain@gmail.com>
2024-05-08 23:48:22 -0400

    Add comments and Move/Copy handling

--- Matrix.h
+++ Matrix.h
@@ -2,8 +2,10 @@
 #ifndef BACKPROPAGATION_MATRIX_H
 #define BACKPROPAGATION_MATRIX_H
 
+#include <algorithm>
 #include <stdexcept>
 #include <iostream>
+#include <string>
 #include <thread>
 
 #ifdef DEBUG_MODE
@@ -18,11 +20,16 @@ const int CONCURRENCY_LIMIT = (int) std::thread::hardware_concurrency();
 template<typename T>
 class Matrix {
 public:
-    int rows{};
-    int cols{};
-    int length;
-    T *data;
+    int rows{}; //NOLINT
+    int cols{}; //NOLINT
+    int length; //NOLINT
+    T *data;    //NOLINT
 
+    /**
+     * Constructor for the Matrix class
+     * @param rows number of rows
+     * @param cols number of columns
+     */
     Matrix(int rows, int cols) {
         if (rows <= 0 || cols <= 0) {
             throw std::invalid_argument(
@@ -35,6 +42,13 @@ public:
         this->data = new T[length];
     }
 
+    /**
+     * Constructor for the Matrix class. Copies the data from the pointer. Does not copy the pointer.
+     * You are free to delete the pointer after this.
+     * @param rows number of rows
+     * @param cols number of columns
+     * @param data pointer to the data
+     */
     Matrix(int rows, int cols, T *data) {
         this->rows = rows;
         this->cols = cols;
@@ -51,26 +65,74 @@ public:
         }
     }
 
+    /**
+     * Copy constructor
+     * @param other matrix to copy from
+     */
+    Matrix(const Matrix<T> &other) {
+        if (this->rows != other.rows || this->cols != other.cols) {
+            throw std::invalid_argument("Matrix dimensions do not match." +
+                                        std::to_string(this->rows) + "x" + std::to_string(this->cols) + " and " +
+                                        std::to_string(other.rows) + "x" + std::to_string(other.cols) +
+                                        " respectively.");
+        }
+        this->rows = other.rows;
+        this->cols = other.cols;
+        length = other.length;
+        this->data = new T[length];
+        set(other.data);
+    }
+
+    Matrix(Matrix &&other) noexcept {
+        std::cout << "Move constructor called\n";
+        this->rows = other.rows;
+        this->cols = other.cols;
+        length = other.length;
+        this->data = other.data;
+        other.data = nullptr;
+    }
+
+    /**
+     * Fills the matrix with the given value
+     * @param value value to fill the matrix with
+     */
     void fill(T value) {
         for (int i = 0; i < length; i++) {
             data[i] = value;
         }
     }
 
+    /**
+     * Sets the matrix with the given data. Copies the data from the pointer. Does not copy the pointer.
+     * You are free to delete the pointer after this.
+     * @param input pointer to the data
+     */
     void set(T *input) {
         for (int i = 0; i < length; i++) {
             this->data[i] = input[i];
         }
     }
 
+    /**
+     * Fills the matrix with 0s
+     */
     void fill0() {
         fill(0);
     }
 
+    /**
+     *
+     * @param index row to access
+     * @return a pointer to the row
+     */
     auto operator[](long index) {
         return (data + (index * cols));
     }
 
+    /**
+     * @param other matrix to compare with
+     * @return true if the matrices are equal, false otherwise
+     */
     bool operator==(const Matrix<T> &other) const {
         if (rows != other.rows || cols != other.cols) {
             return false;
@@ -85,19 +147,67 @@ public:
         return true;
     }
 
-#ifdef DEBUG_MODE
-    bool operator==(const gsl_matrix &other) const {
-        for (int i = 0; i < length; i++) {
-            if (data[i] != other.data[i]) {
-                std::cout << "Mismatch at index: " << i << " Expected: " << other.data[i] << " Got: " << data[i]
-                          << std::endl;
-                return false;
-            }
+    /**
+     * Copy assignment operator
+     * @param other matrix to copy from
+     * @return reference to the new matrix
+     */
+    Matrix &operator=(const Matrix<T> &other) {
+        std::cout << "Copy assignment operator called" << std::endl;
+        if (this == &other) {
+            return *this;
         }
-        return true;
+        if (this->rows != other.rows || this->cols != other.cols) {
+            throw std::invalid_argument("Matrix dimensions do not match." +
+                                        std::to_string(this->rows) + "x" + std::to_string(this->cols) + " and " +
+                                        std::to_string(other.rows) + "x" + std::to_string(other.cols) +
+                                        " respectively.");
+        }
+        set(other.data);
+        return *this;
     }
+
+    /**
+     * Move assignment operator
+     * @param other matrix to move from
+     * @return reference to the new matrix
+     */
+    Matrix &operator=(Matrix<T> &&other) noexcept {
+        std::cout << "Move assignment operator called\n";
+        if (this == &other) {
+            return *this;
+        }
+        if (this->rows != other.rows || this->cols != other.cols) {
+            throw std::invalid_argument("Matrix dimensions do not match." +
+                                        std::to_string(this->rows) + "x" + std::to_string(this->cols) + " and " +
+                                        std::to_string(other.rows) + "x" + std::to_string(other.cols) +
+                                        " respectively.");
+        }
+        delete[] this->data;
+        std::swap(data, other.data);
+        return *this;
+    }
+
+#ifdef DEBUG_MODE
+    /**
+     * @param gsl_matrix to compare with
+     * @return true if the matrices are equal, false otherwise
+     */
+        bool operator==(const gsl_matrix &other) const {
+            for (int i = 0; i < length; i++) {
+                if (data[i] != other.data[i]) {
+                    std::cout << "Mismatch at index: " << i << " Expected: " << other.data[i] << " Got: " << data[i]
+                              << std::endl;
+                    return false;
+                }
+            }
+            return true;
+        }
 #endif
 
+    /**
+     * Fills the matrix with random values. Range is from INT_MIN to INT_MAX
+     */
     void fillRandom() {
         RandomT<T> rand;
         for (int i = 0; i < length; i++) {
@@ -105,13 +215,18 @@ public:
         }
     }
 
+    /**
+     * Prints the matrix
+     */
     void print() {
         std::cout << "Matrix: " << rows << "x" << cols << "\n";
         for (int i = 0; i < rows; i++) {
             std::cout << "[";
             for (int j = 0; j < cols; j++) {
                 std::cout << data[i * cols + j];
-                if (j < cols - 1) std::cout << ", ";
+                if (j < cols - 1) {
+                    std::cout << ", ";
+                }
             }
             std::cout << "]";
             std::cout << "\n";
@@ -124,14 +239,22 @@ public:
     }
 };
 
+/**
+ * Multiplies two matrices
+ * @tparam T Type of the first matrix
+ * @tparam U Type of the second matrix
+ * @param a Input matrix 1
+ * @param b Input matrix 2
+ * @return The result of the matrix multiplication. The return type is:  \code Matrix &lt;decltype(T{} * U{})&gt; \endcode
+ */
 template<typename T, typename U>
-auto matrix_multiply(const Matrix<T> &a, const Matrix<U> &b) {
+Matrix<decltype(T{} * U{})> matrix_multiply(const Matrix<T> &a, const Matrix<U> &b) {
     if (a.cols != b.rows || a.rows != b.cols) {
         throw std::invalid_argument("Matrix dimensions do not match." +
                                     std::to_string(a.rows) + "x" + std::to_string(a.cols) + " and " +
                                     std::to_string(b.rows) + "x" + std::to_string(b.cols) + " respectively.");
     }
-    Matrix<T> result(a.rows, b.cols);
+    Matrix<decltype(T{} * U{})> result(a.rows, b.cols);
     matrix_multiply_solve_for_range_internal(&a, &b, &result, 0, result.length - 1);
     return result;
 }
@@ -156,20 +279,26 @@ matrix_multiply_solve_for_range_internal(const Matrix<T> *a,
     }
 }
 
+/**
+ * Multiplies two matrices in parallel on the CPU
+ * @tparam T Type of the first matrix
+ * @tparam U Type of the second matrix
+ * @param a Input matrix 1
+ * @param b Input matrix 2
+ * @return The result of the matrix multiplication. The return type is:  \code Matrix &lt;decltype(T{} * U{})&gt; \endcode
+ */
 template<typename T, typename U>
-auto matrix_multiply_parallel(const Matrix<T> &a, const Matrix<U> &b) {
+Matrix<decltype(T{} * U{})> matrix_multiply_parallel(const Matrix<T> &a, const Matrix<U> &b) {
     if (a.cols != b.rows) {
         throw std::invalid_argument("Matrix dimensions do not match." +
                                     std::to_string(a.rows) + "x" + std::to_string(a.cols) + " and " +
                                     std::to_string(b.rows) + "x" + std::to_string(b.cols) + " respectively.");
     }
-    Matrix<T> result(a.rows, b.cols);
+    Matrix<decltype(T{} * U{})> result(a.rows, b.cols);
     result.fill(69);
     const long long n = result.length - 1;
     long long noOfSolutionsPerThread = n / CONCURRENCY_LIMIT;
     noOfSolutionsPerThread++;
-    // noOfSolutionsPerThread = 1;
-    // noOfSolutionsPerThread = std::max(noOfSolutionsPerThread, 1LL);
     ThreadPool pool;
     pool.Start();
     auto *aPtr = &a;
@@ -191,7 +320,7 @@ auto matrix_multiply_parallel(const Matrix<T> &a, const Matrix<U> &b) {
 }
 
 template<typename T, typename U>
-auto matrix_multiply_internal(const Matrix<T> *a, const Matrix<U> *b, long x, long y) {
+decltype(T{} * U{}) matrix_multiply_internal(const Matrix<T> *a, const Matrix<U> *b, long x, long y) {
     decltype(T{} * U{}) ans = 0; // Auto doesn't work here for some reason
     for (int i = 0; i < a->cols; i++) {
         ans += a->data[x * a->cols + i] * b->data[i * b->cols + y];
