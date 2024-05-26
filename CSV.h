448586f6b076ae5f89877c1aa3db2cae28f04458
Amulya Jain <amuly.jain@gmail.com>
2024-05-25 21:04:03 -0400

    Optimised

--- CSV.h
+++ CSV.h
@@ -2,23 +2,26 @@
 #define BACKPROPAGATION_CSV_H
 
 #include "Matrix.h"
+#include <algorithm>
 #include <string>
 #include <fstream>
 #include <ostream>
 #include <stdexcept>
 #include <sys/types.h>
 #include <vector>
-#include <sstream>
+#include <cstdlib>
 
 template<typename T>
 Matrix<T> CsvToMatrix(const std::string &filename, int skipRow = 0, int skipCol = 0, int noRows = -1,
-                      const char &delimiter = ',') {
+                      const char delimiter = ',') {
     std::ifstream file(filename);
     if (!file.is_open()) {
         throw std::runtime_error("Could not open file");
     }
+
     std::vector<std::vector<T>> data;
     std::string line;
+
     while (std::getline(file, line)) {
         if (skipRow > 0) {
             skipRow--;
@@ -28,26 +31,36 @@ Matrix<T> CsvToMatrix(const std::string &filename, int skipRow = 0, int skipCol 
             break;
         }
         noRows--;
+
         std::vector<T> row;
-        std::stringstream ss(line);
-        std::string cell;
+        const char *linePtr = line.c_str();
+        const char *endPtr = linePtr + line.size();
         int temp = skipCol;
-        while (std::getline(ss, cell, delimiter)) {
+
+        while (linePtr < endPtr) {
+            const char *nextDelim = std::find(linePtr, endPtr, delimiter);
             if (temp > 0) {
                 temp--;
-                continue;
+            } else {
+                T value = static_cast<T>(std::strtod(linePtr, nullptr));
+                row.push_back(value);
             }
-            row.push_back(std::stod(cell));
+            if (nextDelim == endPtr) {
+                break;
+            }
+            linePtr = nextDelim + 1;
         }
-        data.push_back(row);
+        data.push_back(std::move(row));
     }
     file.close();
+
     if (data.empty()) {
         throw std::runtime_error("No data found in file");
     }
+
     Matrix<T> matrix(data.size(), data[0].size());
-    for (uint i = 0; i < data.size(); i++) {
-        for (uint j = 0; j < data[0].size(); j++) {
+    for (uint i = 0; i < data.size(); ++i) {
+        for (uint j = 0; j < data[i].size(); ++j) {
             matrix[i][j] = data[i][j];
         }
     }
